528a1b25edeef0b47078034fb3afe7bff538b02a
Abdullah Imran <122538243+17abd12@users.noreply.github.com>
2025-12-06 12:39:12 +0500

    Updating gemini model

--- pages/api/assess.js
+++ pages/api/assess.js
@@ -37,7 +37,7 @@ Patient Info:
 `;
 
   try {
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash-latest' })
+    const model = genAI.getGenerativeModel({ model: 'gemini-2.5-flash' })
     const result = await model.generateContent(prompt);
     const text = result.response.text();
     
